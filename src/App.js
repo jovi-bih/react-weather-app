0b0fef6df0f3fb8bee529ef972b6a0c4483d6ba1
jovi-bih <99941708+jovi-bih@users.noreply.github.com>
2022-07-14 12:46:20 -0400

    Added footer, Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by {""}
-        <a href="#" target="-blank">
-          Bih Jovi{""}
-        </a>
-        {""}
-        and is {""}
-        <a href="#" target="-blank">
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by {""}
+          <a href="#" target="-blank">
+            Bih Jovi
+          </a>
+          {""} and is {""}
+          <a href="#" target="-blank">
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
