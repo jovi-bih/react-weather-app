3c4ee59d489aae8f593236fafc6134fac792cc6e
jovi-bih <99941708+jovi-bih@users.noreply.github.com>
2022-07-13 21:02:17 -0400

    Added the footer

--- src/App.js
+++ src/App.js
@@ -3,5 +3,20 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by {""}
+        <a href="#" target="-blank">
+          Bih Jovi{""}
+        </a>
+        {""}
+        and is {""}
+        <a href="#" target="-blank">
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
